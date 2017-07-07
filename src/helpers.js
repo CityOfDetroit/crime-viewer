1b22f004bdedc6b0f72532f6f785ad76ae7da4c2
jessicamcinchak <jessica.mcinchak@gmail.com>
2017-07-07 11:10:02 -0400

    remove example helper fnc

--- src/helpers.js
+++ src/helpers.js
@@ -4,11 +4,6 @@ var _ = require('lodash');
 const Helpers = {
   xDaysAgo: function(int) {
     return moment().subtract(int, 'days').toISOString().slice(0,10)
-  },
-
-  /* Just showin' how it works, delete this later &*/
-  example: function(string) {
-    return string;
   }
 }
 
