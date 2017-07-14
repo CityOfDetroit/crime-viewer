c2b8b4f2e68c71c5cc3e14934cec8546859e4705
Juan Carlos <juanthewebdeveloper@gmail.com>
2017-07-14 14:07:04 -0400

    added beta banner, added comments in javascript

--- src/main.js
+++ src/main.js
@@ -149,7 +149,10 @@ map.on('load', function() {
 
 jQuery(document).ready(function() {
 
+  //responsively adjust height of tab content
   var currentHeight = jQuery('#menu').height() - jQuery('.logo').height() - jQuery('.search').height() - jQuery('.tab-links').height();
+
+  //initiate scrollbar
   jQuery('.scrollbar-macosx').scrollbar();
   jQuery('.scroll-wrapper.tab-content').height(currentHeight - 10);
   jQuery(window).resize(function () {
@@ -175,11 +178,12 @@ jQuery(document).ready(function() {
   //initialize accordion
   jQuery('#filters-accordion [data-accordion]').accordion();
 
+  //close disclaimer box
   jQuery('.disclaimer-close img').click(function(){
     jQuery('.disclaimer').fadeOut();
   });
 
-
+  //initiate slideout
   var slideout = new Slideout({
     'panel': document.getElementById('map'),
     'menu': document.getElementById('menu'),
