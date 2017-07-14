8783f9b7e5f5876a44dc8896fbc309d6e0c9829d
Jimmy McBroom <james.mcbroom@gmail.com>
2017-07-14 09:59:23 -0400

    filter by category, first pass

--- src/filter.js
+++ src/filter.js
@@ -1,7 +1,32 @@
+import Data from './data.js'
+
 const Filter = {
   /* read the UI inputs and make the object */
   readInput: function() {
-    return null
+    let filterObject = {
+      // offense code
+      'state_offense_code': [],
+      // time
+      'hour_of_day': [],
+      'day_of_week': [],
+      // location
+      'neighborhood': [],
+      'precinct': [],
+      'zip_code': [], 
+      'council_district': [],
+
+    }
+    const categoryInputs = ['violent-check', 'property-check', 'other-check']
+    categoryInputs.forEach(i => {
+      let elem = document.getElementById(i)
+      if(elem.checked) {
+        let type = elem.id.split('-')[0]
+        Data.offenses[type].forEach(o => {
+          filterObject['state_offense_code'] = filterObject['state_offense_code'].concat(o['state_codes'])
+        })
+      }
+    })
+    return filterObject
   },
 
   /* make a mapbox-gl filter from an object of filters */
@@ -10,6 +35,7 @@ const Filter = {
     // we may want this to be "any"; possibly a toggle somewhere
     let mapboxFilter = ["all"]
     Object.entries(obj).forEach(([k, v]) => {
+      if(v.length < 1) { return; s}
       let inList = ["in", k]
       mapboxFilter.push(inList.concat(v))
     })
